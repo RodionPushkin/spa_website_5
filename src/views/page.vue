fa5a025f18b10f04ad4e0a305df2815091084ad8
Rodion <50911976+Tequila1Boy@users.noreply.github.com>
2022-01-06 20:36:13 +0300

    Initial commit

---
+++ src/views/page.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page">
+    <h1>ты чмо!</h1>
+  </div>
+</template>
+<script>
+import gsap from 'gsap'
+export default {
+  mounted(){
+    this.axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    .then((response) => {
+      console.log(response.data);
+    })
+    gsap.to(document.querySelector('h1'), {
+      y: 100
+    })
+  }
+}
+</script>
