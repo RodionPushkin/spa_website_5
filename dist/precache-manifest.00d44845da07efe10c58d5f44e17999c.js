a4bb5611f3bf3b1c0c86a88a6ff7ab269fabd0c2
Tequila1Boy <pushkin.rodion@yandex.ru>
2022-01-07 13:14:01 +0300

    build

---
+++ dist/precache-manifest.00d44845da07efe10c58d5f44e17999c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6deb35c54612b67525d",
+    "url": "/spa_website_5/css/app.4f8e9fcd.css"
+  },
+  {
+    "revision": "945ce9f23ce438a69898929f7c97e3d1",
+    "url": "/spa_website_5/img/1.945ce9f2.png"
+  },
+  {
+    "revision": "1799ed9d4ca95fc288e282e0038275b8",
+    "url": "/spa_website_5/img/2.1799ed9d.png"
+  },
+  {
+    "revision": "ef5fa472f6ca6d6edea9ed3353df60e3",
+    "url": "/spa_website_5/img/3.ef5fa472.png"
+  },
+  {
+    "revision": "2d47eb53d0a37fc39bbcf709ce9aee46",
+    "url": "/spa_website_5/img/shum.2d47eb53.png"
+  },
+  {
+    "revision": "9f13da7eb7686ec65d6c8e021ba9ef0f",
+    "url": "/spa_website_5/index.html"
+  },
+  {
+    "revision": "a6deb35c54612b67525d",
+    "url": "/spa_website_5/js/app.36ab993c.js"
+  },
+  {
+    "revision": "8d3278453d8eb313dead",
+    "url": "/spa_website_5/js/chunk-2d0b5e12.83e1bd98.js"
+  },
+  {
+    "revision": "56d96d7d073dc61a277e",
+    "url": "/spa_website_5/js/chunk-2d226194.bcec3632.js"
+  },
+  {
+    "revision": "7388600f9becebb4d944",
+    "url": "/spa_website_5/js/chunk-vendors.501b6419.js"
+  },
+  {
+    "revision": "31cde321b9a73bcb2cf0196924a03f36",
+    "url": "/spa_website_5/manifest.json"
+  },
+  {
+    "revision": "2fb820932cf0a144284f3bcd4eca5067",
+    "url": "/spa_website_5/media/pexels-rostislav-uzunov-9150545.2fb82093.mp4"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/spa_website_5/robots.txt"
+  }
+]);
